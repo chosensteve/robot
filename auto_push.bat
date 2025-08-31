f5fd2eaf5eb2cac08ef593dfdca11497f17b759a
chosensteve <chosenstevegamingyt@gmail.com>
2025-08-31 21:15:11 +0530

    31-08-2025 21:15:11.74

---
+++ auto_push.bat
@@ -0,0 +1,4 @@
+cd C:\Users\Dev\Desktop\cs
+git add .
+git commit -m "%date% %time%"
+git push origin main
--- trest.py
+++
@@ -1,7 +0,0 @@
-test = int(input("input: "))
-
-if test == 1:
-    print("1")
-
-else:
-    print (":(")
