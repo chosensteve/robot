742de0b61c84fe7bc9ee377c827f82fe5717b826
chosensteve <chosenstevegamingyt@gmail.com>
2026-04-16 14:44:40 +0530

    16-04-2026 14:44:39.81

--- 12th project/main.py
+++ 12th project/main.py
@@ -9,7 +9,7 @@ sel_table = ''
 
 def connect():
     try:
-        q = sq.connect(host='localhost', user=user_v, password=password_v, charset='UTF8', db=data_base)
+        q = sq.connect(host='localhost', user=user_v, password=password_v, charset='utf8', db=data_base)
         cur = q.cursor()
     except Exception as e:
         print(e)
@@ -30,7 +30,7 @@ def login():
         user_v = input("username: ")
         password_v = input("password: ")
         try:
-            q = sq.connect(host='localhost', user=user_v, password=password_v, charset='UTF8')
+            q = sq.connect(host='localhost', user=user_v, password=password_v, charset='utf8')
         except Exception as n:
             print("Incorrect Username or Password")
         else:
@@ -56,20 +56,21 @@ def sql_menu():
             q, cur = connect()
 
             if c == 0:
+                close(q, cur)
                 sys.exit()
             
-            if c == 1:
+            elif c == 1:
                 r = cur.execute("show databases;")
                 for i in cur.fetchall():
                     print(i[0], end='\n\n')
             
-            if c == 2:
+            elif c == 2:
                 r = cur.execute(f"create database {input("Database name: ")};")
             
-            if c == 3:
+            elif c == 3:
                 r =  cur.execute(f"drop database {input("Database name: ")};")
             
-            if c == 4:
+            elif c == 4:
                 global data_base
                 data_base = input("Database name: ")
                 r = cur.execute(f"use {data_base};")
@@ -81,39 +82,40 @@ def sql_menu():
         finally:
             close(q, cur)
 
-
 def db_menu():
-    print("\n0. Exit")
-    print("1. Show Tables")
-    print("2. Setup Tables")
-    print("3. Create Custom Table")
-    print("4. Delete Table")
-    print("5. Select Table")
+    while True:
+        print("\n0. Exit")
+        print("1. Show Tables")
+        print("2. Setup Tables")
+        print("3. Create Custom Table")
+        print("4. Delete Table")
+        print("5. Select Table")
 
-    c = input("\tChoice: ")
-    print()
-    
-    try:
-        q, cur = connect()
-    
-        if c == 0:
-            sys.exit() 
-        if c == 1:
-            r = cur.execute("show tables;")
-            for i in cur.fetchall():
-                print(i[0])
-        if c == 2:
-            close(q, cur)
-            setup_tables()
-        if c == 3:
-            print("Work In Progress")
-        if c == 4:
-            r =  cur.execute(f"drop table {input("Table name: ")};")
-        #if c == 5:
-    except Exception as n:
-        print(n)
-    finally:
-        close(q, cur)   
+        c = input("\tChoice: ")
+        print()
+        
+        try:
+            q, cur = connect()
+        
+            if c == 0:
+                close(q, cur)
+                sys.exit() 
+            elif c == 1:
+                r = cur.execute("show tables;")
+                for i in cur.fetchall():
+                    print(i[0])
+            elif c == 2:
+                close(q, cur)
+                setup_tables()
+            elif c == 3:
+                print("Work In Progress")
+            elif c == 4:
+                r =  cur.execute(f"drop table {input("Table name: ")};")
+            #if c == 5:
+        except Exception as n:
+            print(n)
+        finally:
+            close(q, cur)   
 
 
 
